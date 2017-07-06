da27bb23fc8bdaf7ca5592ff9cb08c422342fe0b
Andvri-G42 <javier.martinez@unet.edu.ve>
2017-07-05 20:35:54 -0400

    add webpack conf / hamsa class

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var pkg = require ('./package.json');
+
+module.exports = {
+    cache:true,
+    resole:{ extension : ['','.jsx','.js'] },
+    context: __dirname,
+    entry:{ app: ['webpack/hot/dev-server', './app.jsx' ] },
+    output:{
+        path: './build',
+        filename: pkg.name + '.[name],js',
+        publicPath: '/build/'
+    },
+    devServer:{
+        host: '0.0.0.0',
+        port: 8080,
+        inline:true
+    },
+    module:{
+        loaders:[
+            {
+                test: /(\.js|.jsx)$/,
+                loader: 'babel',
+                query: { presents: ['es2015', 'stage-0', 'react']}
+            }
+        ]
+    }
+};
